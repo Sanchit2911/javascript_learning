869746d3c3c66179ac03ed161906505867da3b5a
Sanchit <sanchitkumar2911@gmail.com>
2023-12-26 01:03:13 +0530

    02_dataTypes completed

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // strict mode --> treat all JS code as newer version(global scope)
+
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3 
+    + 
+      3) // code readibility should be high
+
+console.log("Sanchit")
+
+// Primitve Data Types
+
+let name = "Sanchit" // string => " "
+let age = 18 // number => 2 to the power of 53
+let isLoggedIn = false // boolean => true/false
+let state; // undefined
+let city = null
+
+// null => standalone value
+// symbol => unique
+
+
+// Non primitive data type => object
+
+// typeof operator tells the data type
+
+console.log(typeof age)  // number
+console.log(typeof "Sanchit") // string
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
